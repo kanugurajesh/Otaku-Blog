e16954823bac78b8831e2f3e0c3414d818e6ba39
kanugurajesh <kanugurajesh3@gmail.com>
2024-09-30 18:54:19 +0530

    Adding the base project

---
+++ contentlayer.config.ts
@@ -0,0 +1,30 @@
+import { defineDocumentType, makeSource } from "contentlayer2/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "**/*.mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    date: {
+      type: "date",
+      required: true,
+    },
+    summary: {
+      type: "string",
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => {
+        return `/posts/${post._raw.flattenedPath}`;
+      },
+    },
+  },
+}));
+
+export default makeSource({ contentDirPath: "posts", documentTypes: [Post] });
