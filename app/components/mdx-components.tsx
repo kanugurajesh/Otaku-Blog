26eeb5b688acdc61068c8d23b064a2946d0afc7a
kanugurajesh <kanugurajesh3@gmail.com>
2024-09-30 22:45:59 +0530

    Adding the complete blog

--- app/components/mdx-components.tsx
+++ app/components/mdx-components.tsx
@@ -1,10 +1,28 @@
-import { getMDXComponent } from "next-contentlayer2/hooks";
+import { useMDXComponent } from "next-contentlayer2/hooks";
+
+const components = {
+  h1: ({ ...props }) => (
+    <h1 className="text-xl text-center font-semibold mb-8" {...props} />
+  ),
+  h2: ({ ...props }) => (
+    <h2 className="text-lg font-semibold mt-6 mb-4" {...props} />
+  ),
+  h3: ({ ...props }) => (
+    <h3 className="text-base font-semibold mt-4 mb-2" {...props} />
+  ),
+  blockquote: ({ ...props }) => (
+    <blockquote
+      className="border-l-4 border-blue-500 pl-4 py-2 my-4"
+      {...props}
+    />
+  ),
+};
 
 interface MdxProps {
   code: string;
 }
 
 export default function Mdx({ code }: MdxProps) {
-  const Content = getMDXComponent(code);
-  return <Content />;
+  const Content = useMDXComponent(code);
+  return <Content components={components} />;
 }
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -27,9 +27,9 @@ export default function PostPage({ params }: PostPageProps) {
   }
 
   return (
-    <article className="max-w-xl mx-auto flex flex-col gap-5 my-7 items-start">
+    <article className="max-w-xl mx-auto flex flex-col gap-10 my-7 items-start">
       <div className="flex items-center flex-row-reverse gap-6 justify-center">
-        <div>
+        <div className="flex gap-1 flex-col">
           <h1 className="text-3xl font-bold text-center text-blue-500">
             {post.title}
           </h1>
