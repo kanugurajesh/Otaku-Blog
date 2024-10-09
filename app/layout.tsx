42f824e6fdec6e9dadf95ab88b86da56fc0200ad
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-09 15:00:31 +0530

    Adding the gradient background

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="absolute top-0 -z-10 h-full w-full bg-white"><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div></div>
         {children}
       </body>
     </html>
